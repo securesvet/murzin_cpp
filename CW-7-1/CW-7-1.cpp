37b9a801c0500029102b049b5448297e624a679f
Svet M <67125915+securesvet@users.noreply.github.com>
2022-06-12 20:17:22 +0500

    [CW-7-1] Вспомогательные функции

--- CW-7-1/CW-7-1.cpp
+++ CW-7-1/CW-7-1.cpp
@@ -5,52 +5,55 @@
 #include <vector>
 #include <string>
 
+// Функция  для ввода vector<int>
 std::vector<int> vectorInput(int sizeOfVector) {
-    std::vector<int> vec;
-    //vec.reserve(sizeOfVector);
+    std::vector<int> vectorInteger;
 
     for (int i = 0; i < sizeOfVector; ++i) {
-        int input_num;
-        std::cin >> input_num;
-        vec.push_back(input_num);
+        int inputNum;
+        std::cin >> inputNum;
+        vectorInteger.push_back(inputNum);
     }
 
-    return vec;
-} // Функция  для ввода vector<int>
+    return vectorInteger;
+}
 
-std::vector<int> vectorStoi(std::vector<std::string> vec) {
-    std::vector<int> vecInt;
-    for (int i = 0; i < vec.size(); ++i) {
-        vecInt.at(i) = std::stoi(vec.at(i));
+// Функция для перевода vector<std::string> ---> vector<int>
+std::vector<int> vectorStringToInteger(std::vector<std::string> inputVectorString) {
+    std::vector<int> vectorInteger;
+    for (int i = 0; i < inputVectorString.size(); ++i) {
+        vectorInteger.at(i) = std::stoi(inputVectorString.at(i));
     }
-    return vecInt;
-} // Функция для перевода vector<std::string> ---> vector<int>
+    return vectorInteger;
+}
 
-std::vector<std::string> vectorItos(std::vector<int> inputVector) {
-    std::vector<std::string> vectorString(inputVector.size());
-    for (int i = 0; i < inputVector.size(); ++i) {
-        vectorString.at(i) = std::to_string(inputVector.at(i));
+// Функция для перевода vector<int> ---> vector<std::string>
+std::vector<std::string> vectorIntegerToString(std::vector<int> inputVectorInteger) {
+    std::vector<std::string> vectorString(inputVectorInteger.size());
+    for (int i = 0; i < inputVectorInteger.size(); ++i) {
+        vectorString.at(i) = std::to_string(inputVectorInteger.at(i));
     }
     return vectorString;
-} // Функция для перевода vector<int> ---> vector<std::string>
+}
 
-std::string join(std::vector<std::string> inputVector, char separator) {
+// Функция принимает вектор и разделитель, возвращает строку, где элементы стоят через разделитель
+std::string join(std::vector<std::string> inputVectorString, char separator) {
     std::string vectorSeparated;
-    for (int i = 0; i < inputVector.size() - 1; ++i) {
-        vectorSeparated += inputVector.at(i) + separator;
+    for (int i = 0; i < inputVectorString.size() - 1; ++i) {
+        vectorSeparated += inputVectorString.at(i) + separator;
     }
 
-    vectorSeparated += inputVector.at(inputVector.size()-1); // Чтобы не было вывода наподобие: 1_2_3_ Последний элемент я добавляю вручную
+    vectorSeparated += inputVectorString.at(inputVectorString.size() - 1); // Чтобы не было вывода наподобие: 1_2_3_ Последний элемент я добавляю вручную
     return vectorSeparated;
-} // Функция принимает вектор и разделитель, возвращает строку, где элементы стоят через разделитель
+}
 
 template<typename T> // Нужен, чтобы выводить vector<int> и vector<std::string> одной и той же функцией vectorPrint
-
+// Функция для вывода вектора
 void vectorPrint(std::vector<T> inputVector) {
     for (int i = 0; i < inputVector.size(); ++i) {
         std::cout << inputVector.at(i) << " ";
     }
-} // Функция для вывода вектора
+}
 
 int main() {
     std::vector<int> inputVectorInt;
@@ -59,12 +62,12 @@ int main() {
     vectorPrint(inputVectorInt);
 
     std::vector<std::string> inputVectorString;
-    inputVectorString = vectorItos(inputVectorInt); //
+    inputVectorString = vectorIntegerToString(inputVectorInt); //
 
-    std::string vecSeparated;
-    vecSeparated = join(inputVectorString, '_');
+    std::string vectorSeparated;
+    vectorSeparated = join(inputVectorString, '_');
 
-    std::cout << vecSeparated << '\n'; // Вывод строки с разделителем #: 1_2_3
+    std::cout << vectorSeparated << '\n'; // Вывод строки с разделителем #: 1_2_3
 
     vectorPrint(inputVectorInt); // Вывод работает для vector<int> inputVectorInt
     vectorPrint(inputVectorString); // Вывод работает для vector<std::string> inputVectorString
