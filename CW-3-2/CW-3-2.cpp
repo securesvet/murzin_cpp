f0e92b819998425756f9fe702eeb710d777d1e11
Svet M <67125915+securesvet@users.noreply.github.com>
2022-06-16 18:53:16 +0500

    [CW-3-2] Минимальный палиндром

--- CW-3-2/CW-3-2.cpp
+++ CW-3-2/CW-3-2.cpp
@@ -5,43 +5,30 @@
 #include <string>
 
 int main() {
-
     std::string inputString;
-
     std::cin >> inputString;
-
-    std::string palindromString = inputString;
-
-    std::string palindromMinimum = inputString;
+    std::string palindromeString = inputString;
+    std::string palindromeMinimum = inputString;
 
     for (int i = 0; i < inputString.length() - 2; ++i) {
-
         if (inputString[i] == inputString[i + 1]) {
-
-            palindromString = inputString.substr(i, 2);
-
+            palindromeString = inputString.substr(i, 2);
         } else {
-
             if (inputString[i] == inputString[i + 2]) {
-
-                palindromString = inputString.substr(i, 3);
-
+                palindromeString = inputString.substr(i, 3);
             }
         }
-
-        if (palindromString < palindromMinimum) { // если palindromString меньше palindromMinimum
-            palindromMinimum = palindromString;
+        if (palindromeString < palindromeMinimum) { // если palindromeString меньше palindromeMinimum
+            palindromeMinimum = palindromeString;
         }
     }
-
     // Чтобы не было out of range при проверке равенства знаков
     // Вручную проверяем последние два знака
     if (inputString[inputString.length() - 2] == inputString[inputString.length() - 1] &&
-        inputString.substr(inputString.length() - 2, 2) < palindromMinimum) {
-        palindromMinimum = inputString.substr(inputString.length() - 2, 2);
+        inputString.substr(inputString.length() - 2, 2) < palindromeMinimum) {
+        palindromeMinimum = inputString.substr(inputString.length() - 2, 2);
     }
-
-    std::cout << palindromMinimum;
+    std::cout << palindromeMinimum;
 
     return 0;
 }
