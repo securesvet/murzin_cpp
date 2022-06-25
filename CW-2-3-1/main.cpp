9c5470a636f8258c764ec4a6f754d8d56042452e
Svet M <67125915+securesvet@users.noreply.github.com>
2022-06-25 21:24:52 +0500

    Конфликты с названиями, надо переписать

--- CW-2-3-1/main.cpp
+++ CW-2-3-1/main.cpp
@@ -1,5 +1,21 @@
-#include "Node.h"
+#include "Graph.h"
+#include "LinkedListQueue.h"
+#include "LinkedListStack.h"
 
+// Обход графа в ширину
+int breadthFirstSearch(Graph *graph) {
+    // Выделяем место для очереди
+    LinkedListQueue *queue = new LinkedListQueue;
+
+
+}
+
+// Обход графа в глубину
+int depthFirstSearch(Graph *graph) {
+    // Выделяем место для стека
+    LinkedListStack *stack = new LinkedListStack;
+
+}
 int main() {
 
 }
